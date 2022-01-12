8e205e660e55feff7089bb0b9b9ce7eefa755425
Riyad Nahin <riyadnahin1@gmail.com>
2022-01-12 22:45:54 +0600

    Final

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
 import AboutUs from "./components/AboutUs/AboutUs";
 import Friends from "./components/Friends/Friends";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      {/* <Home></Home> */}
       <BrowserRouter>
-        <Switch>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/about">
-            <AboutUs></AboutUs>
-          </Route>
-          <Route path="/notFound">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/about" element={<AboutUs />}></Route>
+          <Route path="/notFound" element={<NotFound />}></Route>
+          <Route path="/" element={<Navigate to="/home" />}></Route>
+        </Routes>
       </BrowserRouter>
     </div>
   );
